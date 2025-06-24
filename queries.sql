e4d79f4323f19104f6e5a99087ae917992003294
AnnaSvertkova <svertkovahunter28@mail.ru>
2025-06-24 23:15:46 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -2,10 +2,8 @@ select count(customer_id) as customers_count
 from customers;
 
 select
-    -- имя и фамилия продовца
     concat(employees.first_name, ' ', employees.last_name) as seller,
-    count(sales.sales_person_id) as operations, --количество сделок 
-    --суммарная выручка продавца за все время
+    count(sales.sales_person_id) as operations,
     floor(sum(sales.quantity * products.price)) as income
 from employees
 inner join sales on employees.employee_id = sales.sales_person_id
@@ -16,12 +14,10 @@ limit 10;
 
 with tab as (
     select floor(avg(sales.quantity * products.price)) as average_all
-    --средняя выручка за сделку по всем продавцам
     from sales
     inner join products on sales.product_id = products.product_id
 )
 select
-    -- имя и фамилия продавца
     concat(employees.first_name, ' ', employees.last_name) as seller,
     floor(avg(sales.quantity * products.price)) as average_income
 from employees
@@ -32,10 +28,8 @@ having avg(sales.quantity * products.price) < (select average_all from tab)
 order by average_income;
 
 select
-    -- имя и фамилия продfвца
     concat(employees.first_name, ' ', employees.last_name) as seller,
     to_char(sales.sale_date, 'day') as day_of_week,
-    --суммарная выручка продавца
     floor(sum(sales.quantity * products.price)) as income
 from employees
 inner join sales on employees.employee_id = sales.sales_person_id
@@ -48,8 +42,8 @@ select
         when (age) between 16 and 25 then '16-25'
         when (age) between 26 and 40 then '26-40'
         when (age) > 40 then '40+'
-    end as age_category, --возрастные группы
-    count(customer_id) as age_count  --количество покупателей
+    end as age_category,
+    count(customer_id) as age_count
 from customers
 group by age_category
 order by age_category;
@@ -65,16 +59,14 @@ order by selling_month;
 
 with tab as (
     select
-        --имя и фамилия покупателя
         sales.sale_date,
         concat(customers.first_name, ' ', customers.last_name) as customer,
-        -- имя и фамилия продавца
         concat(employees.first_name, ' ', employees.last_name) as seller
     from sales
     left join customers on sales.customer_id = customers.customer_id
     left join employees on sales.sales_person_id = employees.employee_id
     left join products on sales.product_id = products.product_id
-    where products.price = 0 --акционные товары отпускали со стоимостью равной 0
+    where products.price = 0
     order by customers.customer_id
 ),
 tab2 as (
